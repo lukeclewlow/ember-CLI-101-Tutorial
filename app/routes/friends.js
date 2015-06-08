23616f73ae6f600e56cf050514f8927b1ab78d21
Luke Clewlow <luke.clewlow@gmail.com>
2015-06-08 16:20:21 +0100

    created naive validation of input fields

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	actions: {
+		save: function() {
+			console.log('+--- save action bubbled up to friedns route');
+
+			return true;
+		},
+		cancel: function() {
+			console.log('+--- cancel action bubbled up to friends route');
+
+			return true;
+		}
+	}
 });
